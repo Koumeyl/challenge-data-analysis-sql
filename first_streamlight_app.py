dfee674e9dddbaa4498c47252f00be68654fc06b
KB <koumeyl-belkhidar@becode.xyz>
2022-10-26 10:12:49 +0200

    Adding non value on first case

--- first_streamlight_app.py
+++ first_streamlight_app.py
@@ -2,17 +2,19 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 import sqlite3
+
+from case_three import percentage_of_companies_per_type
 conn = sqlite3.connect("Database/bce.db")
 c = conn.cursor()
 
-st.title('Get precentage of Jur')
+st.title('Get precentage of JuridicalForm')
 df = pd.read_sql_query("SELECT JuridicalForm FROM enterprise", conn)
-chart_data = pd.DataFrame(df["JuridicalForm"].value_counts(normalize=True, sort = True) * 100).reset_index()
+chart_data = pd.DataFrame(df["JuridicalForm"].value_counts(normalize=True, sort = True, dropna=False) * 100).reset_index()
 chart_data.rename({'index': 'JuridicalForm', 'JuridicalForm': 'Percentage'}, axis=1, inplace=True)
-chart_data_greatest = chart_data[chart_data["Percentage"] > 1]
-c = alt.Chart(chart_data_greatest).mark_bar().encode(
+c = alt.Chart(chart_data).mark_bar().encode(
     x=alt.X('JuridicalForm', sort=None),
     y='Percentage',
 )
-
 st.altair_chart(c, use_container_width=True)
+
+# percentage_of_companies_per_type(conn)
